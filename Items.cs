03d836b79ff2cfc1a2f3a1cbd5ae155800f81a8e
dittan <32952128+dittan@users.noreply.github.com>
2017-10-24 13:00:19 +0200

    Class added

---
+++ Items.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt1
+{
+    class Items
+    {
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public double TotalPrice { get; set; }
+        public int ProductQuantity { get; set; }
+
+        public double GetTotalPrice(double total, double price)
+        {
+
+        }
+    }
+}
