c0e72a31b923370d5ca17c4d538ab649bd88d7d7
dittan <32952128+dittan@users.noreply.github.com>
2017-10-25 10:58:02 +0200

    Revert "Revert "Item""
    This reverts commit a42b49494045baa4a2475085273a70014c234153.

--- Items.cs
+++ Items.cs
@@ -11,24 +11,18 @@ namespace Projekt1
 { 
     class Items : MyForm
     { 
-        List<Product>ItemList { get; set; }
+        public List<Product>ItemList { get; set; }
     }
-    class Product
+    class Product : Items
     {
-        public string Name { get; set; }
+        public string ItemName { get; set; }
         public string Info { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
     }
 
-    public void CreatProduct(string name, string info, double price, int quantity)
+    public void AddProduct(Product product)
     {
-        new Product
-        {
-            Name = name,
-            Info = info,
-            Price = price,
-            Quantity = quantity
-        };
+        
     }
 }
--- MyForm.cs
+++ MyForm.cs
@@ -134,6 +134,7 @@ namespace Projekt1
 
             grid1.CellContentClick += Grid1_CellContentClicked;
             grid2.CellContentClick += Grid2_CellContentClicked;
+            
         }
 
         private void Grid1_CellContentClicked(object sender, DataGridViewCellEventArgs e)
