7dbf644f934e849dc473851a3416f70bc3655fc3
dittan <32952128+dittan@users.noreply.github.com>
2017-10-25 10:57:29 +0200

    Revert "Items change"
    This reverts commit 4a7e005840bebacfb26e2df8a66f58e9ee5b1530.

--- Items.cs
+++ Items.cs
@@ -7,28 +7,30 @@ using System.Windows.Forms;
 using System.Drawing;
 using System.IO;
 
-namespace Projekt1
-{ 
+namespace Projekt1 
+{
+    
     class Items : MyForm
     { 
-        List<Product>ItemList { get; set; }
-    }
-    class Product
-    {
-        public string Name { get; set; }
-        public string Info { get; set; }
-        public double Price { get; set; }
-        public int Quantity { get; set; }
-    }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public int ProductQuantity { get; set; }
+        public List<Items> ItemsList { get; set; } 
 
-    public void CreatProduct(string name, string info, double price, int quantity)
-    {
-        new Product
+        public void GetTotalPrice()
         {
-            Name = name,
-            Info = info,
-            Price = price,
-            Quantity = quantity
-        };
+            foreach(DataGridViewRow row in grid2.Rows)
+            {
+                new Items
+                {
+                ProductName = Convert.ToString(row.Cells[1].Value),
+                ProductQuantity = Convert.ToInt32(row.Cells[3].Value),
+                ProductPrice = Convert.ToDouble(row.Cells[4].Value),
+                };
+                //itemlistadd
+
+                
+            } 
+        }
     }
 }
