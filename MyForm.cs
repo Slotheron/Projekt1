60e30c88b1939aa5bb50d396682170dcdbbc2c36
dittan <32952128+dittan@users.noreply.github.com>
2017-10-31 11:40:51 +0100

    Pics and stuff

--- MyForm.cs
+++ MyForm.cs
@@ -18,6 +18,7 @@ namespace Projekt1
  
         public MyForm()
         {
+            Text = "Welcome To Triple-J:s Webbshop";
             products = new List<Product> { };
             var table = (new TableLayoutPanel
             {
@@ -82,7 +83,7 @@ namespace Projekt1
             //C:\Users\Joakim\Documents\GitHub\Projekt1\Products.txt
             //C:\Users\Joe\source\repos\Projekt1\Projekt1\Products.txt
             //C:\Users\Jacob\Documents\GitHub\Projekt1\Products.txt
-            string path = @"C:\Users\JoeKH_000\Documents\GitHub\Projekt1\products.txt"; /* products list location  @"";*/
+            string path = @"C:\Users\Jacob\Documents\GitHub\Projekt1\Products.txt"; /* products list location  @"";*/
             string[] lines = File.ReadAllLines(path);
 
             //loop to grab values from a text file to create Products or Items.
